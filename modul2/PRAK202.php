ee9db43f61168159159c7766439303b399ed5176
MSNatama <101536511+MSNatama@users.noreply.github.com>
2022-03-23 17:01:38 +0800

    Adding color to error msg

--- modul2/PRAK202.php
+++ modul2/PRAK202.php
@@ -24,9 +24,12 @@
     }
     ?>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        Nama: <input type="text" name="nama" value="<?php echo $nama ?>"><span class="error">* <?php echo $nameErr ?> </span><br>
-        NIM: <input type="text" name="nim" value="<?php echo $nim ?>"><span class="error">* <?php echo $nimErr ?> </span><br>
-        jenis kelamin: <span class="error">* <?php echo $genderErr ?> </span><br>
+        Nama: <input type="text" name="nama" value="<?php echo $nama ?>">
+        <span class="error" style="color:red">* <?php echo $nameErr ?> </span><br>
+        NIM: <input type="text" name="nim" value="<?php echo $nim ?>">
+        <span class="error" style="color:red">* <?php echo $nimErr ?> </span><br>
+        jenis kelamin: 
+        <span class="error" style="color:red">* <?php echo $genderErr ?> </span><br>
         <input type="radio" name="gender" <?php if (isset($gender) && $gender == "Laki-Laki") echo "checked"; ?> value="Laki-Laki">Laki-laki <br>
         <input type="radio" id="p" name="gender" <?php if (isset($gender) && $gender == "Perempuan") echo "checked"; ?> value="Perempuan">Perempuan<br>
         <input type="submit" name="enter" value="submit">
